d6c40cfb0135c13b35540f1de88a271f66da59a7
limsangyeop <149994184+limsangyeop@users.noreply.github.com>
2023-11-22 16:42:53 +0900

    first commit
    Company "Draw" MFC Coding Test

---
+++ CDlgImage.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CDlgImage 대화 상자
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+	CWnd* m_pParent;
+	CImage m_MainScreen;
+	CPoint m_ptData;
+	int m_Pos =0;
+	CRect m_rect;
+	int m_Switch = 0;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CDlgImage };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	void DrawMainScreen();
+	afx_msg void OnPaint();
+	void DrawCircle(CDC* pDC);
+	void DrawMidLine(CDC* pDC);
+	void DrawCheckCircle(CDC* pDC);
+	void DrawFullCircle(CDC* pDC);
+};
---
+++ MFC_DrawDlg.h
@@ -0,0 +1,46 @@
+﻿
+// MFC_DrawDlg.h: 헤더 파일
+//
+
+#pragma once
+#include "CDlgImage.h"
+
+
+// CMFCDrawDlg 대화 상자
+class CMFCDrawDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCDrawDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	CDlgImage* m_MainScreen;
+	//int nRadius;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_DRAW_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedShowscreen();
+	afx_msg void OnBnClickedDrawcircleBt();
+	afx_msg void OnBnClickedWhereposBt();
+	afx_msg void OnBnClickedDrawlineBt();
+	afx_msg void OnBnClickedCheckBt();
+	afx_msg void OnBnClickedDrawfullcircle();
+	afx_msg void OnEnSetfocusInput();
+};
